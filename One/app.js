739ed689f2d81e7c6970915b7e48b0dce161c37b
VaibhavAna <vaibhavanand2494@gmail.com>
2026-06-09 01:48:57 +0530

    Update

--- One/app.js
+++
@@ -1,8 +0,0 @@
-let a=10;
-let b="20";
-console.log(typeof(a+b));
-
-//Diff Num and Strings.
-
-// "10"+"1"= "101" ->String
-//When ever we add two string it is called concatination not addition
---
+++ One/index.js
@@ -0,0 +1,89 @@
+// let a=10;
+// let b="20";
+// // console.log(typeof(a+b));
+
+// //Diff Num and Strings.
+
+// // "10"+"1"= "101" ->String
+// //When ever we add two string it is called concatination not addition.
+//  console.log("sum of 10 and 20" (+a+b));
+
+
+
+//Type coercion.
+// String ko no mai convert kr deyga.
+// Ex-
+// Console.log("1"-1);
+// It will print 0 because its only function is to subtract .
+
+// console.log("1"+1);
+
+//* will do work 
+// + It can do 2 work Addition and concation 
+// and addition can perform only when two no is Present
+
+// let age = Number(prompt("What is your age"));
+// console.log(typeof age);
+
+//prompt make string output 
+
+//Type casting 
+
+//converting into any any data from another like string to Number.
+
+
+//*****Swapping*****
+
+//1 Method:-Swap with extra variable.
+
+// let a=10;
+// let b=20;
+// let c;
+
+// c=a;
+// a=b;
+// b=c;
+
+// console.log(a);
+// console.log(b);
+
+//So output will be a=20 and b=10 
+
+// 2.Method:- Swap two variable without using extra variable .
+
+// let a= 10;
+// let b=20;
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log(a,b);
+
+//3.Method:-Destructing 
+
+// let a =10;
+// let b=20 ;
+
+// [a,b]= [b,a]
+
+// console.log(a,b);
+
+//Operators 
+// 1.Arthmatic:-+,-,/,%,*
+
+// console.log(a/b);
+
+// Math.floor we use so we can get single number without integer (without pont ex-2.1234505)
+// divison sign qutioned. 
+//mod will always give reminder.
+
+// 2.Relational operators
+// 3.logical operators
+// 4.Unary operators
+
+
+
+
+
+
